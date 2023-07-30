1ff7781c854c1aecfeb54a100a76b327911b90d6
Josh Carlson <magneticstain@gmail.com>
2023-07-29 21:52:38 -0400

    Add unit test for awsconnector package

---
+++ src/aws_connector/aws_connector_test.go
@@ -0,0 +1,18 @@
+package awsconnector_test
+
+import (
+	"reflect"
+	"testing"
+
+	awsconnector "github.com/magneticstain/ip2cr/src/aws_connector"
+)
+
+func TestConnectToAWS(t *testing.T) {
+	ac := awsconnector.AWSConnector{}
+
+	acType := reflect.TypeOf(ac.AwsConfig)
+
+	if acType.Name() != "Config" {
+		t.Errorf("AWS connector failed to connect; wanted aws.Config type, received %s", acType.Name())
+	}
+}
