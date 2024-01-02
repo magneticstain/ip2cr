94f97ee1f784c9281cf3dcb0ff1e1896104277fd
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-01-01 21:34:05 -0500

    Revert "Disable rollbar wrapper to make profiling clearer"
    This reverts commit 36426fa9368dcf6b7d67cc8bb41953744f5c6560.

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 	"strings"
 
+	"github.com/rollbar/rollbar-go"
 	log "github.com/sirupsen/logrus"
 
 	awsconnector "github.com/magneticstain/ip-2-cloudresource/aws_connector"
 	"github.com/magneticstain/ip-2-cloudresource/resource"
 	"github.com/magneticstain/ip-2-cloudresource/search"
+	"github.com/magneticstain/ip-2-cloudresource/utils"
 )
 
 const APP_VER = "v1.2.0"
@@ -150,11 +152,9 @@ func main() {
 
 	log.Info("starting IP-2-CloudResource")
 
-	// utils.InitRollbar(APP_VER)
+	utils.InitRollbar(APP_VER)
 
-	// rollbar.WrapAndWait(runCloudSearch, *ipAddr, *cloudSvc, *ipFuzzing, *advIPFuzzing, *orgSearch, *orgSearchXaccountRoleARN, *orgSearchRoleName, *orgSearchOrgUnitID, *networkMapping, *silentOutput, *jsonOutput)
+	rollbar.WrapAndWait(runCloudSearch, *ipAddr, *cloudSvc, *ipFuzzing, *advIPFuzzing, *orgSearch, *orgSearchXaccountRoleARN, *orgSearchRoleName, *orgSearchOrgUnitID, *networkMapping, *silentOutput, *jsonOutput)
 
-	// rollbar.Close()
-
-	runCloudSearch(*ipAddr, *cloudSvc, *ipFuzzing, *advIPFuzzing, *orgSearch, *orgSearchXaccountRoleARN, *orgSearchRoleName, *orgSearchOrgUnitID, *networkMapping, *silentOutput, *jsonOutput)
+	rollbar.Close()
 }
