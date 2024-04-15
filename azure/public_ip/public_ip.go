20ba6f543b2c8eb13b59009de477637acf59c8c3
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-04-15 15:45:13 -0400

    Move public IP fetching function to own struct
    Allows usage of it by vms and (now) LBs. It's likely to be needed for CDNs as well.

---
+++ azure/public_ip/public_ip.go
@@ -0,0 +1,37 @@
+package publicip
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
+	log "github.com/sirupsen/logrus"
+)
+
+type AzPublicIPAddr struct {
+	AzureConn azidentity.DefaultAzureCredential
+}
+
+func GetPublicIPAddressProperties(azureConn *azidentity.DefaultAzureCredential, publicIpData *armnetwork.PublicIPAddress, ctx context.Context) (armnetwork.PublicIPAddressesClientGetResponse, error) {
+	var pubIpAddrClient *armnetwork.PublicIPAddressesClient
+	var publicIpAddrProps armnetwork.PublicIPAddressesClientGetResponse
+	var err error
+
+	log.Debug("fetching public IP address properties directly for ", *publicIpData.ID)
+
+	parsedPublicIpId, err := arm.ParseResourceID(*publicIpData.ID)
+	if err != nil {
+		return publicIpAddrProps, err
+	}
+
+	pubIpAddrClient, err = armnetwork.NewPublicIPAddressesClient(parsedPublicIpId.SubscriptionID, azureConn, nil)
+	if err != nil {
+		return publicIpAddrProps, err
+	}
+
+	// yes, it actually does match by name vs ID
+	publicIpAddrProps, err = pubIpAddrClient.Get(ctx, parsedPublicIpId.ResourceGroupName, parsedPublicIpId.Name, nil)
+
+	return publicIpAddrProps, err
+}
