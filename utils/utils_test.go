9dec08fa2536312c36267588d62db52a56a2eba7
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-04-27 22:59:40 -0400

    Update `example.com` IPs in utils unit tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -48,10 +48,10 @@ func TestLookupFQDN(t *testing.T) {
 		fqdn, ipAddr    string
 		expectedVerdict bool
 	}{
-		{"example.com", "93.184.216.34", true},                       // accurate IPv4 lookup
-		{"example.com", "1.1.1.1", false},                            // inaccurate IPv4 lookup
-		{"example.com", "2606:2800:220:1:248:1893:25c8:1946", true},  // accurate IPv6 lookup
-		{"example.com", "2600:9000:24eb:3a00:1:3b80:4f00:21", false}, // inaccurate IPv6 lookup
+		{"example.com", "93.184.215.14", true},                          // accurate IPv4 lookup
+		{"example.com", "1.1.1.1", false},                               // inaccurate IPv4 lookup
+		{"example.com", "2606:2800:21f:cb07:6820:80da:af6b:8b2c", true}, // accurate IPv6 lookup
+		{"example.com", "2600:9000:24eb:3a00:1:3b80:4f00:21", false},    // inaccurate IPv6 lookup
 	}
 
 	for _, td := range tests {
